9d9baf222011f4749a606530ec49e725187a0c35
pratyush-bhatnagar3009 <64519337+pratyush-bhatnagar3009@users.noreply.github.com>
2021-03-17 12:33:32 +0530

    Add files via upload

---
+++ AppDrawerNavigator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { AppTabNavigator } from './AppTabNavigator'
+import CustomSideBarMenu  from './CustomSideBarMenu';
+import MyDonationScreen from '../screens/MyDonationScreen';
+import NotificationScreen from '../screens/NotificationScreen';
+import SettingScreen from '../screens/SettingScreen';
+import MyReceivedBooksScreen from '../screens/MyReceivedBooksScreen';
+import {Icon} from 'react-native-elements'
+
+export const AppDrawerNavigator = createDrawerNavigator({
+  Home : {
+    screen : AppTabNavigator,
+    navigationOptions:{
+      drawerIcon : <Icon name="home" type ="fontawesome5" />
+    }
+    },
+  MyDonations : {
+    screen : MyDonationScreen
+  },
+  Notification : {
+    screen : NotificationScreen
+  },
+  MyReceivedBooks :{
+    screen: MyReceivedBooksScreen
+  },
+  Setting : {
+    screen : SettingScreen
+  }
+},
+  {
+    contentComponent:CustomSideBarMenu
+  },
+  {
+    initialRouteName : 'Home'
+  })
